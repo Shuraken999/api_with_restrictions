e5581c8719c92ebe6e7d972cd9b286c490111e44
bobclub1996 <70020651+bobclub1996@users.noreply.github.com>
2023-08-26 04:55:51 +0400

    Фильтрация по дате есть, но не работает 2.0

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -15,10 +15,10 @@ class AdvertisementViewSet(ModelViewSet):
     #   сериализаторов и фильтров
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, AdvertisementFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['creator', 'created_at']
     permission_classes = [IsAuthenticated, IsOwner]
-
+    ordering_fields = ['created_at']
 
     # def get_permissions(self):
     #     """Получение прав для действий."""
